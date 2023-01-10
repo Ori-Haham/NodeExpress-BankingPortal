a65b4b2b783ce733b84794a3df84153b5bf3038c
Ori-Haham <ori.haham100@gmail.com>
2023-01-10 16:07:53 +0200

    module1

---
+++ src/app.js
@@ -0,0 +1,22 @@
+const fs = require('fs');
+const path = require('path');
+const express = require('express');
+
+const app = express();
+const router = express.Router();
+
+var viewPath = path.join(__dirname, './src/views');
+
+app.use(express.static(path.join(__dirname, '/public/')));
+
+router.route('/').get((req, res) => {
+  res.render('index', { title: 'Index' });
+});
+
+app.set('views', viewPath);
+
+app.set('view engine', 'ejs');
+
+app.server = app.listen(PORT, () => {
+  console.log(`api project listening on port: ${PORT}`);
+});
