f6b45a7dd1521d3f43512fb898684a2954c0c111
Ori-Haham <ori.haham100@gmail.com>
2023-01-11 12:25:50 +0200

    nodule2

--- src/app.js
+++ src/app.js
@@ -6,6 +6,12 @@ const app = express();
 const router = express.Router();
 const port = process.env.PORT || 3000;
 
+const accountData = fs.readFileSync('./json/accounts.json', 'utf8');
+const accounts = JSON.parse(accountData);
+
+const userData = fs.readFileSync('./json/accounts.json', 'utf8');
+const users = JSON.parse(userData);
+
 var viewPath = path.join(__dirname, './views');
 
 app.set('views', viewPath);
@@ -14,15 +20,25 @@ app.set('view engine', 'ejs');
 
 app.use(express.static(path.join(__dirname, '/public/')));
 
-const accountData = fs.readFileSync('./json/accounts.json', 'utf8');
-const accounts = JSON.parse(accountData);
-
-const userData = fs.readFileSync('./json/accounts.json', 'utf8');
-const users = JSON.parse(userData);
-
 function routerFunction() {
   router.route('/').get((req, res) => {
     res.render('index', { title: 'Account Summary', accounts });
+  });
+
+  router.route('/savings').get((req, res) => {
+    res.render('account', { account: accounts.savings });
+  });
+
+  router.route('/checking').get((req, res) => {
+    res.render('account', { account: accounts.checking });
+  });
+
+  router.route('/credit ').get((req, res) => {
+    res.render('account', { account: accounts.credit });
+  });
+
+  router.route('/profile ').get((req, res) => {
+    res.render('profile', { user: users[0] });
   });
 
   return router;
